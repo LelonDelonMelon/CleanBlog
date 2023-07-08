01b0127187590437f62cb6d60492dca17772711b
LelonDelonMelon <burkinggs@gmail.com>
2023-07-09 02:33:46 +0300

    Patika 2nd Task

--- app.js
+++ app.js
@@ -2,10 +2,20 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-const blog = { id: 1, title: 'Blog title', description: 'Blog description' };
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-  res.send(blog);
+  res.render('index');
+});
+app.get('/about', (req, res) => {
+  res.render('about');
+});
+app.get('/add_post', (req, res) => {
+  res.render('add_post');
+});
+app.get('/post', (req, res) => {
+  res.render('post');
 });
 
 app.listen(3000, () => {
